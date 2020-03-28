da9c15778e2c991acf0d7f0c127601f8a9402f7a
Gabriel Goncalves <goncalves.gabrielsilva@gmail.com>
2020-03-28 14:38:20 -0300

    Swagger

--- src/main/java/br/com/covid19api/controller/CovidHistoricalController.java
+++ src/main/java/br/com/covid19api/controller/CovidHistoricalController.java
@@ -2,12 +2,17 @@ package br.com.covid19api.controller;
 
 import br.com.covid19api.provider.receivers.CoronaLmaoConsumer;
 import br.com.covid19api.service.CovidHistoricalService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api()
 @RestController
 @RequestMapping("historicos")
 public class CovidHistoricalController {
@@ -19,7 +24,12 @@ public class CovidHistoricalController {
     private CoronaLmaoConsumer receiver;
 
     @GetMapping("{country}")
-    public void getHistorical(@PathVariable("country") String country){
+    @ApiOperation(value = "Apresenta informações do país em comparativo com outros país passado", consumes = "application/json",httpMethod = "GET")
+    @ApiResponse(code = 200, message = "Busca do comparativo realizado com sucesso")
+    public void getHistorical(
+        @PathVariable("country")
+        @ApiParam(value = "Nomes dos países para comparação")
+        String country){
         receiver.consume(country);
     }
 
